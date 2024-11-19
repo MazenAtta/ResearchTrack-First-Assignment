2c48f438bd364e1b797a81fa543b325f45255d31
MazenAtta <mazenatta445@gmail.com>
2024-11-19 12:14:29 +0100

    Third commit

--- src/Distance.cpp
+++ src/Distance.cpp
@@ -9,6 +9,11 @@ ros::Publisher distance_pub;
 ros::Publisher turtle1_pub;
 ros::Publisher turtle2_pub;
 turtlesim::Pose turtle1_pose, turtle2_pose;
+std_msgs::Float32 distance_msg;
+
+
+
+
 geometry_msgs::Twist stop_msg;
 geometry_msgs::Twist move_msg;
 
@@ -30,33 +35,41 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "turtle_distance");
     ros::NodeHandle n;
 
+
     distance_pub = n.advertise<std_msgs::Float32>("/turtle_distance", 10);
     turtle1_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
     turtle2_pub = n.advertise<geometry_msgs::Twist>("/turtle2/cmd_vel", 10);
 
+
     ros::Subscriber turtle1_sub = n.subscribe("/turtle1/pose", 10, turtle1Callback);
     ros::Subscriber turtle2_sub = n.subscribe("/turtle2/pose", 10, turtle2Callback);
     
-    // Reset state flags at the start
-    turtles_stop_flag = 0;
-    turtle1_boundary_stop_flag = 0;
-    turtle2_boundary_stop_flag = 0;
-    
-    distance = std::sqrt(std::pow(turtle1_pose.x - turtle2_pose.x, 2) +
-                               std::pow(turtle1_pose.y - turtle2_pose.y, 2));
 
-    std_msgs::Float32 distance_msg;
-    distance_msg.data = distance;
-    distance_pub.publish(distance_msg);
+    // Wait for initial pose updates
+    ROS_INFO("Waiting for turtle1 and turtle2 pose updates...");
+    ros::Rate rate(10); // 10 Hz loop
+    bool poses_received = false;
+    while (ros::ok() && !poses_received) {
+        ros::spinOnce(); // Process callbacks
 
-    while (ros::ok()) {
-    
+        // Check if both poses are non-zero (indicating they've been updated)
+        if (turtle1_pose.x != 0.0 || turtle1_pose.y != 0.0 || turtle2_pose.x != 0.0 || turtle2_pose.y != 0.0) {
+            poses_received = true;
+        }
+        rate.sleep();
+    }
+    ROS_INFO("Pose updates received. Starting main loop.");
+
+    while (ros::ok()) 
+    {
+    ros::spinOnce();
+
     // Calculate distance between turtles
     distance = std::sqrt(std::pow(turtle1_pose.x - turtle2_pose.x, 2) +
-                               std::pow(turtle1_pose.y - turtle2_pose.y, 2));
-
-    std_msgs::Float32 distance_msg;
+                         std::pow(turtle1_pose.y - turtle2_pose.y, 2));
+    
     distance_msg.data = distance;
+
     distance_pub.publish(distance_msg);
 
     // Stop turtles if they are too close to each other or if turtle1 or turtle2 is too close to the boundary
@@ -87,6 +100,7 @@ int main(int argc, char **argv) {
     else 
     {
         // do nothing
+        ROS_INFO("Turtles distance = %f", distance);
     }
     
 
@@ -132,10 +146,13 @@ int main(int argc, char **argv) {
         ros::Duration(1.0).sleep();
         ROS_WARN("Moving turtle2: to avoid boundary.");
     }
+    else
+    {
+        // do nothing
+    }
 
 
 
-    ros::spinOnce();
 
     }
     return 0;
